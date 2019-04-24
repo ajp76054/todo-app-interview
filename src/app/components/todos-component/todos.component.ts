252a0984ce094335c766e82b8428a6382f1e4c49
ajp76054 <ajp76054@gmail.com>
2019-04-24 18:12:01 -0500

    2 files modified, 1 added

--- src/app/components/todos-component/todos.component.ts
+++ src/app/components/todos-component/todos.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
+  host: {['class']: 'col-12'}
 })
 export class TodoComponent {
 
